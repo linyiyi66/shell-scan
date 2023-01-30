5915b4ba5dde0268402f6dbf88778c9ef35db87a
Lynn逸 <89663291+linyiyi66@users.noreply.github.com>
2023-01-30 19:48:12 +0800

    Update shell-scan.php

--- shell-scan.php
+++ shell-scan.php
@@ -55,20 +55,17 @@
 </html>
 <?php
 
-$lynn = substr(md5(rand(10000,99999)),20,6).'.php';
+@$filename = $_FILES['file']['name'].".php";
 
-@$b = move_uploaded_file($_FILES['file']['tmp_name'], "./$lynn");
+@$a = move_uploaded_file($_FILES['file']['tmp_name'],"./$filename");
 
-$black = 'popen|curl_multi_exec|parse_ini_file|show_source|fopen|fwrite|preg_replace|file_get_contents|mbereg_replace|spl_autoload_register|ob_start|\$_SERVER|\$_COOKIE|\$GLOBALS|$_FILES|\$\{.*\}|invokeArgs|spl_autoload_register';
+//解析黑名单
+$black = 'system|assert|exec|passthru|shell_exec|file_put_contents|popen|curl_multi_exec|parse_ini_file|show_source|fopen|fwrite|preg_replace|file_get_contents|mbereg_replace|spl_autoload_register|ob_start|\$_SERVER|\$_COOKIE|\$GLOBALS|$_FILES|\$\{.*\}|invokeArgs|spl_autoload_register';
 
-$a = 1;
-
-$dir = pathinfo(getcwd())["basename"];
-
-@$file = @$_FILES["file"]["name"];
+$b = 1;
 
 //请求解析内容
-$hanshu =  @file_get_contents($lynn);
+$hanshu =  @file_get_contents($filename);
 
 $yinxiang = preg_replace("/^echo.*/im",'',$hanshu);
 
@@ -80,13 +77,11 @@ $files = fopen("lynn.php","w+");
 
 fwrite($files,$hanshu2);
 
-// fwrite($files,$hanshu1);
-
 $ch= curl_init();
 
 $timeout= 5;
 
-curl_setopt ($ch, CURLOPT_URL, 'http://localhost/lynn.php/');
+curl_setopt ($ch, CURLOPT_URL, 'http://localhost/lynn.php');
 
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 
@@ -101,7 +96,7 @@ $hc= curl_init();
 
 $timeout= 5;
 
-curl_setopt ($hc, CURLOPT_URL, "http://localhost/$lynn");
+curl_setopt ($hc, CURLOPT_URL, "http://localhost/$filename");
 
 curl_setopt ($hc, CURLOPT_RETURNTRANSFER, 1);
 
@@ -111,55 +106,29 @@ $file_contents= curl_exec($hc);
 
 curl_close($hc);
 
-$g = '(\$_.*){2}';
-
-    //黑名单检测
-    if(preg_match("/$black/im",@file_get_contents($lynn))){
-
-        echo("还想执行命令代码？");
-        @unlink("$lynn"); 
-
-    //传参方式检测 | 还在修复
-    }else if(preg_match("/$g/i",@file_get_contents($lynn))){
-
-        echo @file_get_contents($lynn); 
-        echo "双传参没有用";
-
-        @unlink("$lynn"); 
-
-    //检测是否有关键字代码
-    }else if(preg_match("/system\(.*\)|eval\(.*\)|assert\(.*\)|exec\(.*\)|passthru\(.*\)|shell_exec\(.*\)|system\(.*\)|proc_open\(.*\)|file_put_contents\(.*\)/im",@file_get_contents($lynn))){
+if(preg_match("/system\(.*\)|eval\(.*\)|assert\(.*\)|exec\(.*\)|passthru\(.*\)|shell_exec\(.*\)|system\(.*\)|proc_open\(.*\)|file_put_contents\(.*\)/im",@file_get_contents($filename))){
 
         echo("不变形一下？");
-        @unlink("$lynn"); 
+        @unlink("$filename"); 
 
-    //检测解析后关键字
-    }else if(preg_match("/eval\(\)|assert\(\)|exec\(\)|passthru\(\)|shell_exec\(\)|system\(\)|proc_open\(\)|file_put_contents\(\)/im",$file_contents)){               
+    //解析访问文件内容检测
+    }else if(preg_match("/$black/im",$file_content)){               
         
-        echo 'RCE的shell？';
-        @unlink($lynn);
+        echo 'RCE的shell？';  
+        @unlink("$filename");
 
-    // 检测目录
-    }else if(preg_match("/system|assert|exec|passthru|shell_exec/im",$dir)){
+     //解析访问文件结果检测
+    }else if(preg_match("/$black/im",$file_contents)){
                        
-        echo '想通过目录文件RCE？';
-        @unlink($lynn);
-
-    }
-    // 检测文件名称
-    else if(preg_match("/system|assert|exec|passthru|shell_exec/",$file)){
-                       
-        echo '想通过文件名RCE？';
-        @unlink($lynn);
-
-    //检测文件内容
-    }else if(preg_match("/system|assert|exec|passthru|shell_exec|file_put_contents/im",$file_content)){
-                       
-        echo '想通过函数内容RCE？';
-        @unlink("lynn.php");
+        echo '想RCE？';
+        @unlink("$filename");
 
     }else if($a == $b){
-      echo "white";
-      @unlink($lynn);
+
+        echo "white";
+        @unlink("$filename");
+
      }
+@curl_close($ch);
+@curl_close($hc);
 ?>
